047193a2619ca39fd9f97a3dd21f742fd5d10fff
Mikael Kindborg <mikael.kindborg@gmail.com>
2013-12-04 11:11:05 +0100

    EvoThings documentation site, first commit.

---
+++ js/include-css-scripts.js
@@ -0,0 +1,7 @@
+// Include of scripts and CSS-files.
+document.write(
+	'<link href="libs/bootstrap-3.0.2/css/bootstrap.min.css" '
+	+	'rel="stylesheet" media="screen"> '
+	+ '<script src="libs/jquery/jquery-2.0.3.js"></script>'
+	+ '<script src="libs/bootstrap-3.0.2/js/bootstrap.min.js"></script>'
+	+ '<link rel="stylesheet" href="hyper.css" media="screen" />')
---
+++ js/include-main-menu.js
@@ -0,0 +1,12 @@
+// Documentation menu bar.
+document.write(
+	'<div class="hyper-main-menu btn-group btn-group-justified">'
+	+	'<a href="index.html" type="button" '
+	+		'class="btn btn-lg btn-primary">Studio</a>'
+	+	'<a href="workbench.html" type="button" '
+	+		'class="btn btn-lg btn-primary">Workbench</a>'
+	+	'<a href="mobile-app.html" type="button" class="btn btn-lg '
+	+		'btn-primary">Mobile App</a>'
+	+	'<a href="examples.html" type="button" class="btn btn-lg '
+	+		'btn-primary">Examples</a>'
+	+ '</div>')
