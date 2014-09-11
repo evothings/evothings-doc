639861dd39866da581b0ab2ae347865401281542
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-09-11 14:58:52 +0200

    Updated headings to be consistent, fixed bug in footer inclusion, added conditional in footer include script, made menu items mixed case.

--- js/include-footer.js
+++ js/include-footer.js
@@ -3,3 +3,14 @@ document.write(
 	'<div class="box">'
 	+	'<p>Copyright &copy; 2013-2014 Evothings AB</p>'
 	+ '</div>')
+
+
+// When browsing documentation online we call a script
+// to rewrite links to work with the website layout.
+var protocol = window.location.protocol
+if (protocol != 'file:')
+{
+	document.write(
+		'<scr'+'ipt type="text/javascript" '
+		+	'src="http://www.evothings.com/doc/doc-links.js" ></scr'+'ipt>')
+}
