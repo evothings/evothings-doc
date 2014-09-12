8adfb5a40d799d2397db37cb18f0d5b4a7997bec
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-09-12 13:42:04 +0200

    Updated CSS to work with Wordpress.

--- js/include-css-scripts.js
+++ js/include-css-scripts.js
@@ -1,19 +1,7 @@
 // Include scripts and CSS-files.
 ;(function()
 {
-	var path =
-		((window.location.href.indexOf('index.html') > -1) ||
-		 (window.location.href.indexOf('.html') == -1))
-			 ? ''
-			 : '../'
-
-	document.write(
-		'<script src="' + path + 'libs/jquery/jquery-2.0.3.js"></script>'
-		+ '<link rel="stylesheet" href="' + path + 'css/hyper.css" media="screen" />')
-
-	// Only add Google Analytics if documentation is read online.
-	var protocol = window.location.protocol
-	if (protocol != 'file:')
+	function enableGoogleAnalytics()
 	{
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -21,5 +9,32 @@
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-44159401-1', 'auto');
 		ga('send', 'pageview');
+	}
+
+	var protocol = window.location.protocol
+	var raw = window.location.href.indexOf('/raw') > -1
+	var path =
+		((window.location.href.indexOf('index.html') > -1) ||
+		 (window.location.href.indexOf('.html') == -1))
+			 ? ''
+			 : '../'
+
+	// Include JS/CSS common for both offline/raw and for Wordpress.
+	document.write(
+		'<script src="' + path + 'libs/jquery/jquery-2.0.3.js"></script>'
+		+ '<link rel="stylesheet" href="' + path + 'css/evo-doc-content.css" media="screen" />')
+
+	// Include CSS used only for offline/raw.
+	if (raw || protocol == 'file:')
+	{
+		document.write(
+			'<link rel="stylesheet" href="' + path + 'css/evo-doc-raw.css" media="screen" />')
+	}
+
+	// Only add Google Analytics if documentation is read online.
+	// TODO: Disable if not viewed online in raw mode?
+	if (protocol != 'file:')
+	{
+		enableGoogleAnalytics()
 	}
 })()
