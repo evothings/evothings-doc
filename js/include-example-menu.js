4c2e2a1ffc40b221efc0de181d0eac2bc979f0df
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-12-12 14:21:55 +0100

    Updated documentation for release 1.1.0, added documentation for Estimote Scan example.

--- js/include-example-menu.js
+++ js/include-example-menu.js
@@ -2,22 +2,30 @@
 document.write(
 	'<div class="evo-box evo-content-index">'
 	+	'<script src="../js/include-evothings-logo.js"></script>'
+
 	+	'<a href="../examples/hello-world.html">Hello World</a><br/>'
 	+	'<a href="../examples/cordova-basic.html">Cordova Basic</a><br/>'
-	+	'<a href="../examples/arduino-led-onoff-tcp.html">Arduino LED On/Off TCP</a><br/>'
-	+	'<a href="../examples/arduino-led-onoff-ble.html">Arduino LED On/Off BLE</a><br/>'
-	+	'<a href="../examples/arduino-scriptable-tcp.html">Arduino Scriptable</a><br/>'
-	+	'<a href="../examples/arduino-ble.html">Arduino BLE</a><br/>'
-	+	'<a href="../examples/ibeacon-scan.html">iBeacon Scan</a><br/>'
+	+	'<a href="../examples/hue-lights.html">Philips Hue Demo</a><br/>'
+
 	+	'<a href="../examples/ble-scan.html">BLE Scan</a><br/>'
+	+	'<a href="../examples/ibeacon-scan.html">iBeacon Scan</a><br/>'
+	+	'<a href="../examples/estimote-scan.html">Estimote Scan</a><br/>'
 	+	'<a href="../examples/ble-discovery.html">BLE Discovery</a><br/>'
+
 	+	'<a href="../examples/ble-ti-sensortag.html">TI SensorTag Demo</a><br/>'
 	+	'<a href="../examples/ble-ti-sensortag-sensors.html">TI SensorTag Sensors</a><br/>'
-	+	'<a href="../examples/hue-lights.html">Philips Hue Demo</a><br/>'
-	+	'<a href="../examples/rfduino-led-onoff.html">RFduino Led On Off</a><br/>'
-	+	'<a href="../examples/lightbluebean-basic.html">LightBlue Bean Basic</a><br/>'
-	+	'<a href="../examples/bluno-helloworld.html">Bluno Hello World</a><br/>'
-	+	'<a href="../examples/nordic-ble.html">Nordic Led On/Off</a><br/>'
+
+	+	'<a href="../examples/arduino-led-onoff-tcp.html">Arduino LED On/Off TCP</a><br/>'
+	+	'<a href="../examples/arduino-led-onoff-ble.html">Arduino LED On/Off BLE</a><br/>'
+	+	'<a href="../examples/arduino-ble.html">Arduino BLE</a><br/>'
+	+	'<a href="../examples/arduino-scriptable-tcp.html">Arduino Scriptable TCP</a><br/>'
+
 	+	'<a href="../examples/redbearlabs-simplecontrol.html">RedBearLab Simple Control</a><br/>'
 	+	'<a href="../examples/redbearlabs-simplechat.html">RedBearLab Simple Chat</a><br/>'
+
+	+	'<a href="../examples/lightbluebean-basic.html">LightBlue Bean Basic</a><br/>'
+	+	'<a href="../examples/rfduino-led-onoff.html">RFduino Led On Off</a><br/>'
+	+	'<a href="../examples/bluno-helloworld.html">Bluno Hello World</a><br/>'
+	+	'<a href="../examples/nordic-ble.html">Nordic Led On/Off</a><br/>'
+
 	+ '</div>')
