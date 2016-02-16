8730a62133278d45380211e0a9dc501a10a2838a
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-02-16 19:06:15 +0100

    Inserting analytics code in a script tag.

--- js/include-page-footer.js
+++ js/include-page-footer.js
@@ -27,19 +27,21 @@
 	function enableGoogleAnalytics()
 	{
 		// Google Analytics script.
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-44159401-1']);
-		_gaq.push(['_gat._forceSSL']);
-		_gaq.push(['_trackPageview']);
-
-		(function () {
-			var ga = document.createElement('script');
-			ga.type = 'text/javascript';
-			ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(ga, s);
-		})();
+		document.write(
+			'<script type="text/javascript">'
+			+ "var _gaq = _gaq || [];"
+			+ "_gaq.push(['_setAccount', 'UA-44159401-1']);"
+			+ "_gaq.push(['_gat._forceSSL']);"
+			+ "_gaq.push(['_trackPageview']);"
+			+ "(function () {"
+			+ "var ga = document.createElement('script');"
+			+ "ga.type = 'text/javascript';"
+			+ "ga.async = true;"
+			+ "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';"
+			+ "var s = document.getElementsByTagName('script')[0];"
+			+ "s.parentNode.insertBefore(ga, s);"
+			+ "})();"
+			+ "</script>")
 	}
 
 	function includeFooter()
