6598b336cdb885bdb713c3ed07e28c1a6f0ec36c
Andreas Lundquist <lundquist.andreas@gmail.com>
2016-05-11 21:19:37 +0200

    Added documentation for MediaTek LinkIt Smart 7688 example

--- js/include-example-menu.js
+++ js/include-example-menu.js
@@ -34,6 +34,7 @@ document.write(
 
 	+	'<a href="../examples/mediatek-linkit.html">MediaTek&nbsp;Linkit&nbsp;ONE&nbsp;Position</a><br/>'
 	+	'<a href="../examples/mediatek-linkit-connect.html">MediaTek Linkit Connect 7681</a><br/>'
+	+	'<a href="../examples/mediatek-linkit-smart-7688-duo.html">MediaTek&nbsp;LinkIt&nbsp;Smart 7688 Duo</a><br/>'
 	+	'<a href="../examples/esp8266.html">ESP8266</a><br/>'
 	+	'<a href="../examples/lightbluebean-basic.html">LightBlue&nbsp;Bean&nbsp;Basic</a><br/>'
 	+	'<a href="../examples/rfduino-led-onoff.html">RFduino&nbsp;Led&nbsp;On/Off</a><br/>'
